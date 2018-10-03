a7a3256ffe62724be77db53546e98a57cfc21dd9
Alex Bepple <alex@bepple.de>
2018-10-03 16:51:59 +0200

    $ repeat test runs

--- src/navigation-primary.test.js
+++ src/navigation-primary.test.js
@@ -1,5 +1,6 @@
 import __ from 'hamjest'
 import * as wdio from 'webdriverio'
+import * as r from 'ramda'
 
 describe('WD', () => {
   let wd, _wd
@@ -12,10 +13,12 @@ describe('WD', () => {
   beforeEach(() => _wd.newWindow('', '', ''))
   afterEach(() => _wd.close() /*window*/)
 
-  it('reads page title', () => {
-    return wd
-      .url('http://localhost:8080')
-      .getTitle()
-      .then((title) => __.assertThat(title, __.containsString('nginx')))
-  })
+  r.range(0, 20).forEach((i) =>
+    it(`reads page title #${i}`, () => {
+      return wd
+        .url('http://localhost:8080')
+        .getTitle()
+        .then((title) => __.assertThat(title, __.containsString('nginx')))
+    })
+  )
 })
