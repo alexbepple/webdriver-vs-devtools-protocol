66a3565693a4f3e2000eb24087cf13239d215c54
Alex Bepple <alex@bepple.de>
2018-10-06 21:15:10 +0200

    % easily configure sample size

--- src/setup.js
+++ src/setup.js
@@ -1,2 +1,8 @@
+import * as r from 'ramda'
+
 export const url = 'http://localhost:8000'
-export const sampleSize = 20
+const parseInt = (x) => Number.parseInt(x, 10)
+export const sampleSize = r.compose(
+  parseInt,
+  r.defaultTo(1)
+)(process.env.SAMPLE_SIZE)
